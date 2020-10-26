26443855a67c3c1f15f1f739f743502248084b01
Billy Vega <billyvega@me.com>
2020-10-25 22:58:45 -0700

    added template literals

--- js/script.js
+++ js/script.js
@@ -30,12 +30,12 @@ function showPage(list, page) {
          `
                <li class="student-item cf" >
                <div class="student-details">
-               <img class="avatar" src="https://randomuser.me/api/portraits" alt="Profile Picture">
-               <h3>$</h3>
-               <span class="email"></span>
+               <img class="avatar" src="${list[i].picture.medium}" alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
                </div>
                <div class="joined-details">
-               <span class="date"></span>
+               <span class="date">${list[i].registered.date}</span>
                </div>
                </li>
                `
@@ -43,55 +43,31 @@ function showPage(list, page) {
             }
          
          };
-
-      // const ul = document.querySelector('ul');
-      // const li = document.createElement('li');
-      // li.className = "student-item cf";
-      // ul.appendChild(li);
-      
-      // const div = document.createElement('div');
-      // div.className = "student-details";
-      // ul.appendChild(div);
-   
-      
-      // const img = document.createElement('img');
-      // img.className ="avatar"
-      // img.src ="https://randomuser.me/api/portraits/women/25.jpg";
-      // img.alt ="Profile Picture";
-      // div.appendChild(img);
-
-      // const h3 = document.createElement('h3');
-      // div.appendChild(h3);
-
-      // const span = document.createElement('span');
-      // span.className = "email";
-      // div.appendChild(span)
-
-      // const div2 = document.createElement('div');
-      // div2.className = "joined-details";
-      // ul.appendChild(div2);
-
-      // const span2 = document.createElement('span');
-      // span.className = "date";
-      // div2.appendChild(span);
-   
-     
-   
-
-
-   
-        
-
  showPage(data,1);
-
-
-
- 
 
 /*
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+
+function addPagination(list) {
+  const pageCount = Math.ceil(list.length / 9);
+  const ul = document.querySelector('.link-list');
+  ul.innerHTML = '';
+   for (let i = 1; i < pageCount; i+=1){
+      
+      const button = 
+      `<li>
+         <button type="button">${i}</button>
+      </li>`
+      
+      ul.insertAdjacentHTML("beforeend",button);
+   }
+
+
+
+}
+addPagination(data);
 
 
 
